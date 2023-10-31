7bad41457113ccb2c674e6fc1d7485bcdc8861de
yuhui17 <cheongyifui@gmail.com>
2023-10-31 17:55:06 +0800

    Add project files.

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+
+    //https://localhost:portnumber/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        //GET: //https://localhost:portnumber/api/students
+        [HttpGet]
+        public IActionResult Get()
+        {
+            string[] studentNames = new string[] { "Alice", "Bob", "Charlie", "David", "Eva" };
+
+            return Ok(studentNames);
+        }
+    }
+}
---
+++ NZWalks.API/Models/Domain/Difficulty.cs
@@ -0,0 +1,8 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Difficulty
+    {
+        public Guid id {get; set;}
+        public string Name { get; set; }
+    }
+}
