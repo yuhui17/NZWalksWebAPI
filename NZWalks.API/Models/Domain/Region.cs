940c1ab1f112cdcb1fc60c3ddff0380887488cef
yuhui17 <cheongyifui@gmail.com>
2023-10-31 18:17:35 +0800

    01 init

--- NZWalks.API/Models/Domain/Region.cs
+++ NZWalks.API/Models/Domain/Region.cs
@@ -1,8 +1,10 @@
 ﻿namespace NZWalks.API.Models.Domain
 {
-    public class Difficulty
+    public class Region
     {
         public Guid Id {get; set;}
+        public string Code { get; set; }
         public string Name { get; set; }
+        public string? RegionImageUrl { get; set; }
     }
 }
